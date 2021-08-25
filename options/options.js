5426c607973434d90a01dcef14c7d5a7dfe7dc0c
Crowdbotics <team@crowdbotics.com>
2021-08-25 02:16:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "solitary_sky_30091Identifier",
+  url: "https://solitary_sky_30091Identifier.botics.co",
+  api: "https://solitary_sky_30091Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
